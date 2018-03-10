16cf49128d69ad613da5b3f5beca9258cfc3734f
hawk <gzscutlyh@163.com>
2018-03-10 09:28:59 +0800

    add

---
+++ knife-game/src/settings.60405.js
@@ -0,0 +1,74 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "4e1wgR9lVKXqg3Z/BDrOPF": [
+                "Texture/game/combo.png",
+                0
+            ],
+            "18CSylBD9DjpIs1383YrQy": [
+                "Texture/game/橙子_0001s_0012_图层-28-拷贝-2.png",
+                0
+            ],
+            "45NpHJZNxNZ41YnMftyWEp": [
+                "Texture/game/橙子_0007_图层-4-拷贝-3.png",
+                0
+            ],
+            "eaV1o9WAJPwZ0/MCik4bNr": [
+                "Texture/game/橙子_0010_图层-4.png",
+                0
+            ],
+            "9fxciZOhxMC78IihOG2w8g": [
+                "Texture/game/橙子_0011_图层-26.png",
+                0
+            ],
+            "4bmIAOVHpFoZKa1fLCsWR+": [
+                "Texture/game/橙子_0012_图层-24.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/Scene/game.fire",
+    scenes: [
+        {
+            url: "db://assets/Scene/game.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "0a1e6d631": [
+            "43UTLNawdCIqb/45qeL6CF",
+            0,
+            "5dj0Q13UROAZW0CAHKwk+n",
+            "77jTdfhfdHBrg+kNzvrDMo",
+            "83HJvVrdpCt59HjB93MLKk",
+            "cbVQtqvwpLhZYw6ZhTZU10",
+            "d5tHSqm1dJ26sVqOHxnwoH",
+            "f9nHpVc3VC9ohPCup3v48l"
+        ]
+    },
+    orientation: "",
+    uuids: [
+        "558FESQwxFhYfu9xPSuEiY"
+    ],
+    "md5AssetsMap": {
+        "0a/0a1e6d631.json": "a211f",
+        "assets/Texture/game/combo.png": "db8af",
+        "assets/Texture/game/橙子_0001s_0012_图层-28-拷贝-2.png": "97991",
+        "assets/Texture/game/橙子_0007_图层-4-拷贝-3.png": "cf695",
+        "assets/Texture/game/橙子_0010_图层-4.png": "0e1aa",
+        "assets/Texture/game/橙子_0011_图层-26.png": "8ad29",
+        "assets/Texture/game/橙子_0012_图层-24.png": "35c70"
+    }
+};
